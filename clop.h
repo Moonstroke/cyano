bf4ae7afe24e6377954fd67b0b5cc6fa03af4337
joH1 <jiema@live.fr>
2017-12-19 18:44:14 +0100

    Renamed two long options.
    `--border-width` to `--border-size` and `--cell-dim` to `--cell-size`

--- clop.h
+++ clop.h
@@ -36,8 +36,8 @@ static const char *const OPTSTRING = "b:c:h:nr:vw:W";
 static const struct option const LONGOPTS[] = {
 	{"board-width",  required_argument, NULL, 'w'},
 	{"board-height", required_argument, NULL, 'h'},
-	{"border-width", required_argument, NULL, 'b'},
-	{"cell-dim",     required_argument, NULL, 'c'},
+	{"border-size",  required_argument, NULL, 'b'},
+	{"cell-size",    required_argument, NULL, 'c'},
 	{"no-border",    no_argument,       NULL, 'n'},
 	{"update-rate",  required_argument, NULL, 'r'},
 	{"vsync",        no_argument      , NULL, 'v'},
