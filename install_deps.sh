22967898ac0ea43c847934bbfdcee065c09f6bb3
joH1 <jiema@live.fr>
2018-01-04 17:25:40 +0100

    Move apt-get invocations to deps installer
    The *before_install* commands in the travis script are moved to
    `install_deps.sh`

--- install_deps.sh
+++ install_deps.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-function install_dep() {
-	git clone $2 $1
-	( cd $1 && make install )
+function install_git_repo() {
+	git clone "$2" "$1"
+	( cd "$1" && make install )
 }
 
-function clean_dep() {
-	rm -rfv "$1"
+function clean_git_repo() {
+	rm -rf "$1"
 }
 
-install_dep 'log' 'https://github.com/Moonstroke/C-log.git'
-clean_dep 'log'
+
+# Refreshing the APT cache doesn't hurt
+sudo apt-get update
+
+# We obviously need the SDL installed
+sudo apt-get install --yes libasound2-dev libdbus-1-dev libegl1-mesa-dev\
+                           libgl1-mesa-dev libgles2-mesa-dev libglu1-mesa-dev\
+                           libibus-1.0-dev libmirclient-dev libpulse-dev\
+                           libsdl2-2.0-0 libsndio-dev libudev-dev libwayland-dev\
+                           libx11-dev libxcursor-dev libxext-dev libxi-dev\
+                           libxinerama-dev libxkbcommon-dev libxrandr-dev\
+                           libxss-dev libxt-dev libxv-dev libxxf86vm-dev\
+                           libsdl2-dev
+
+install_git_repo 'log' 'https://github.com/Moonstroke/C-log.git'
+clean_git_repo 'log'
