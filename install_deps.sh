bff1762217a1e39c935a4d256ef455beb6d6cb2c
joH1 <jiema@live.fr>
2018-01-04 17:30:11 +0100

    Remove sudo from installation commands
    The script is already run as sudo, no need to call it again.

--- install_deps.sh
+++ install_deps.sh
@@ -11,17 +11,16 @@ function clean_git_repo() {
 
 
 # Refreshing the APT cache doesn't hurt
-sudo apt-get update
+apt-get update
 
 # We obviously need the SDL installed
-sudo apt-get install --yes libasound2-dev libdbus-1-dev libegl1-mesa-dev\
-                           libgl1-mesa-dev libgles2-mesa-dev libglu1-mesa-dev\
-                           libibus-1.0-dev libmirclient-dev libpulse-dev\
-                           libsdl2-2.0-0 libsndio-dev libudev-dev libwayland-dev\
-                           libx11-dev libxcursor-dev libxext-dev libxi-dev\
-                           libxinerama-dev libxkbcommon-dev libxrandr-dev\
-                           libxss-dev libxt-dev libxv-dev libxxf86vm-dev\
-                           libsdl2-dev
+apt-get install -y libasound2-dev libdbus-1-dev libegl1-mesa-dev libgl1-mesa-dev\
+                   libgles2-mesa-dev libglu1-mesa-dev libibus-1.0-dev\
+                   libmirclient-dev libpulse-dev libsdl2-2.0-0 libsndio-dev\
+                   libudev-dev libwayland-dev libx11-dev libxcursor-dev\
+                   libxext-dev libxi-dev libxinerama-dev libxkbcommon-dev\
+                   libxrandr-dev libxss-dev libxt-dev libxv-dev libxxf86vm-dev\
+                   libsdl2-dev
 
 install_git_repo 'log' 'https://github.com/Moonstroke/C-log.git'
 clean_git_repo 'log'
