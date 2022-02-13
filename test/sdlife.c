22a3486f6df843cbdd5fb1e16a9f5d2f8d3a445a
joH1 <jiema@live.fr>
2022-02-13 23:10:13 +0100

    Remove code concerning deleted test case

--- test/sdlife.c
+++ test/sdlife.c
@@ -6,20 +6,16 @@
 extern CUTE_TestCase *case_board;
 extern void build_case_board(void);
 
-extern CUTE_TestCase *case_clop;
-extern void build_case_clop(void);
-
 int main() {
 	CUTE_RunResults **results;
 
 	build_case_board();
-	build_case_clop();
 
-	CUTE_prepareTestSuite(2, case_board, case_clop);
+	CUTE_prepareTestSuite(1, case_board);
 
 	results = CUTE_runTestSuite();
 
-	CUTE_printResults(2, results);
+	CUTE_printResults(1, results);
 
 	return EXIT_SUCCESS;
 }
