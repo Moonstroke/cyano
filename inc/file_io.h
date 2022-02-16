7ef15cd3e88cb478371dfc00b11fad21d2fd221b
joH1 <jiema@live.fr>
2022-02-16 23:49:59 +0100

    Declare file I/O functions in a new header
    The functions read from and write to a file through its path.

---
+++ inc/file_io.h
@@ -0,0 +1,40 @@
+/**
+ * \file "file_io.h"
+ * \author joH1
+ *
+ * \version 0.1
+ *
+ * \brief This file defines a set of functions to encapsulate the communication
+ *        with files from their paths.
+ */
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+
+
+/**
+ * \brief Read the content of the file of given path in a string and return it.
+ *
+ * \note The returned value is allocated on the heap and needs to be freed.
+ *
+ * \param[in] path The path to the source file
+ *
+ * \return The content of the file, or \c NULL if an error occurred
+ */
+char *readFile(const char *path);
+
+
+/**
+ * \brief Write the given text to the file of given path.
+ *
+ * \note If the file does not exist, it will be created, otherwise its content
+ *       will be overwritten.
+ * \param[in] path The path to the destination file
+ * \param[in] text The value to write in the file
+ *
+ * \return \c 0 on success, a negative value on error
+ */
+int writeFile(const char *path, const char *text);
+
+
+#endif /* FILE_IO_H */
