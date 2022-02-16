d951de042f6b4d3f89a627ba0ecf99486c8edf02
joH1 <jiema@live.fr>
2022-02-17 00:01:40 +0100

    Implement readFile in a new source file

---
+++ src/file_io.c
@@ -0,0 +1,34 @@
+#include "file_io.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char *readFile(const char *path) {
+	FILE *file = fopen(path, "r");
+	if (file == NULL) {
+		return NULL;
+	}
+	if (fseek(file, 0, SEEK_END) < 0) {
+		fclose(file);
+		return NULL;
+	}
+	long filesize = ftell(file);
+	if (filesize < 0) {
+		fclose(file);
+		return NULL;
+	}
+	char *text = malloc(filesize + 1);
+	if (text == NULL) {
+		fclose(file);
+		return NULL;
+	}
+	if (fread(text, 1, filesize, file) < filesize && ferror(file)) {
+		free(text);
+		fclose(file);
+		return NULL;
+	}
+	fclose(file);
+	return text;
+}
