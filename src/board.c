1d699af6fbfde1635963a60281d3099ad22d91c2
joH1 <jiema@live.fr>
2022-02-16 23:13:10 +0100

    Move impl of some board functions to other source
    The definitions of the function loadBoard and its helper function are
    moved to an independent source file to distinguish them from the other
    board-related functions.

--- src/board.c
+++ src/board.c
@@ -19,40 +19,6 @@ int initBoard(struct board *b, unsigned int w, unsigned int h, bool wrap) {
 }
 
 
-static inline int initBoardFromRepr(struct board *board, const char *repr,
-                                    bool wrap) {
-	const char *itr = repr;
-	for (; *itr && *itr != '\n'; ++itr);
-	unsigned int width = itr - repr;
-	unsigned int height = 1;
-	/* Stepping width characters is faster, but assumes repr is well-formed */
-	for (; *itr; itr += width) {
-		if (*itr == '\n') {
-			++height;
-		} else {
-			/* We did not fall on a line break: repr is ill-formed */
-			return -2;
-		}
-	}
-	return initBoard(board, width, height, wrap);
-}
-
-int loadBoard(struct board *board, const char *repr, bool wrap) {
-	int rc = initBoardFromRepr(board, repr, wrap);
-	if (rc < 0) {
-		return rc;
-	}
-
-	for (size_t i = 0; repr[i]; ++i) {
-		if (repr[i] == '@') {
-			SET_BIT(board->cells, i, true);
-		} else if (repr[i] != '.' && repr[i] != '\n') {
-			return -3;
-		}
-	}
-	return 0;
-}
-
 void freeBoard(struct board *b) {
 	free(b->cells);
 }
