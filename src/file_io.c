96c7aef44d60d501c7169bc4c50643bdfac1f338
joH1 <jiema@live.fr>
2022-02-24 16:38:30 +0100

    Implement writeFile

--- src/file_io.c
+++ src/file_io.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> /* for strlen */
 #include <unistd.h> /* for access */
 
 
@@ -38,6 +39,20 @@ char *readFile(const char *path) {
 	}
 	fclose(file);
 	return text;
+}
+
+int writeFile(const char *path, const char *text) {
+	FILE *file = fopen(path, "w");
+	if (file == NULL) {
+		return -1;
+	}
+	size_t len = strlen(text);
+	size_t written = fwrite(text, 1, len, file);
+	fclose(file);
+	if (written < len) {
+		return -2;
+	}
+	return 0;
 }
 
 bool isFile(const char *path) {
