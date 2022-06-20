9f12eb5b0e6fe8cdfb3b1d8fe25a8eeced58f6d5
joH1 <jiema@live.fr>
2022-06-20 23:51:56 +0200

    Document new header

--- inc/stringutils.h
+++ inc/stringutils.h
@@ -1,3 +1,12 @@
+/**
+ * \file "stringutils.h"
+ * \author joH1
+ *
+ * \version 0.1
+ *
+ * \brief Declarations of functions manipulating strings; an extension of the
+ *        standard header string.h.
+ */
 #ifndef STRINGUTILS_H
 #define STRINGUTILS_H
 
@@ -6,6 +15,15 @@
 
 
 
+/**
+ * \brief Checks whether the end of the first string corresponds to the second
+ *        one.
+ *
+ * \param[in] s1 The first string
+ * \param[in] s2 The second string (the suffix)
+ *
+ * \return \c true iff the first string ends with the second
+ */
 bool endswith(const char *restrict s1, const char *restrict s2);
 
 
