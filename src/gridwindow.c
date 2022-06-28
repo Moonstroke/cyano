1f83e7f09677cd21d6c83f5f5efb8ba165ddab4b
joH1 <jiema@live.fr>
2022-06-28 21:25:45 +0200

    Fix warning in icon init code
    The icon data pointer is cast to silence the warning about discarded
    const qualifier.
    
    Also, a hard-coded byte size value is changed to a sizeof expression;
    it is better in terms of both good practices and
    readability/understandability.

--- src/gridwindow.c
+++ src/gridwindow.c
@@ -653,8 +653,11 @@ int init_grid_window(struct grid_window *gw, struct grid *grid,
 	gw->border_width = border_width;
 	gw->error_msg[0] = '\0';
 
-	SDL_Surface *icon = SDL_CreateRGBSurfaceFrom(icon_data, ICONSIZE, ICONSIZE, 16, ICONSIZE * 2, 0xf000,
-	                                             0x0f00, 0x00f0, 0x000f);
+	SDL_Surface *icon = SDL_CreateRGBSurfaceFrom((uint16_t*)icon_data,
+	                                             ICONSIZE, ICONSIZE, 16,
+	                                             ICONSIZE * sizeof *icon_data,
+	                                             0xf000, 0x0f00, 0x00f0,
+	                                             0x000f);
 	if (icon != NULL) {
 		SDL_SetWindowIcon(gw->win, icon);
 		SDL_FreeSurface(icon);
