7b1eb443dc5e422c9db8080180c58b0dda04fdfe
joH1 <jiema@live.fr>
2022-07-06 22:54:17 +0200

    (Re)write comments and an expression in copy_bits
    A comment is added to explain why the first bytes of src and dest are
    skipped.
    
    The expression in the if block is simplified.
    
    The comment in the else branch is reworded.

--- src/bits.c
+++ src/bits.c
@@ -6,11 +6,12 @@
 
 void copy_bits(const char *restrict src, size_t src_offset, char *restrict dest,
                size_t dest_offset, size_t length) {
+	/* Skip the full bytes until the first byte that is partially used */
 	src += src_offset >> 3;
 	dest += dest_offset >> 3;
 	src_offset &= 7;
 	dest_offset &= 7;
-	if (((src_offset - dest_offset) & 7) == 0) {
+	if (src_offset == dest_offset) {
 		/* Source and dest share the bit alignment inside bytes: an optimization
 		   is possible by writing the partial first byte, blitting all the full
 		   bytes in a single memcpy call, and filling the final partial byte */
@@ -29,7 +30,7 @@ void copy_bits(const char *restrict src, size_t src_offset, char *restrict dest,
 			SET_BIT(dest + length_bytes, i, GET_BIT(src + length_bytes, i));
 		}
 	} else {
-		/* No usable shared alignment, fallback to naive linear copy */
+		/* No common alignment to leverage, fallback to naïve linear copy */
 		for (size_t i = 0; i < length; ++i) {
 			SET_BIT(dest, dest_offset + i, GET_BIT(src, src_offset + i));
 		}
