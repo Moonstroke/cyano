08805aeb8e66062c412018e373738b5a285a0be2
joH1 <jiema@live.fr>
2022-07-14 14:11:51 +0200

    Optimize copy_bits
    When there are leading or trailing partial bytes, instead of setting
    each bit iteratively, the byte is copied in full to the destination,
    with a proper mask applied to restrict the copy to the intended bits.

--- src/bits.c
+++ src/bits.c
@@ -15,19 +15,24 @@ void copy_bits(const char *restrict src, size_t src_offset, char *restrict dest,
 		/* Source and dest share the bit alignment inside bytes: an optimization
 		   is possible by writing the partial first byte, blitting all the full
 		   bytes in a single memcpy call, and filling the final partial byte */
-		for (size_t i = src_offset; i < 8; ++i) {
-			SET_BIT(dest, i, GET_BIT(src, i));
-		}
-		if (src_offset > 0) {
-			/* If we did write a leading offset to a byte, we increment the
-			   pointers to skip it */
+		if (src_offset != 0) {
+			/* There is a partial byte offset: copy the first byte, masking out
+			   the bits not concerned */
+			char mask = 0xff >> src_offset;
+			dest[0] = src[0] & mask;
+			/* We increment the pointers to skip the leading byte */
 			++src;
 			++dest;
 		}
 		size_t length_bytes = (length - src_offset) >> 3;
-		memcpy(dest, src, length_bytes);
-		for (size_t i = 0; i < (length & 7); ++i) {
-			SET_BIT(dest + length_bytes, i, GET_BIT(src + length_bytes, i));
+		if (length_bytes > 0) {
+			memcpy(dest, src, length_bytes);
+		}
+		length = (length - src_offset) & 7;
+		if (length != 0) {
+			/* There is a trailing partial byte, copy it */
+			char mask = 0xff << (8 - length);
+			dest[length_bytes] = src[length_bytes] & mask;
 		}
 	} else {
 		/* No common alignment to leverage, fallback to naïve linear copy */
