5d5979ed2fd152293003405b9030d67719b3bde0
joH1 <jiema@live.fr>
2022-09-04 21:33:02 +0200

    Add a test case for the rule compilation code
    The test case contains some early definition of tests that do not yet
    pass (the expected values have to be worked out).
    However, the case is already referenced and run in the main test
    source.

---
+++ test/rule.c
@@ -0,0 +1,72 @@
+#include "grid.h"
+
+#include <CUTE/cute.h>
+
+
+
+/* The grid to run tests on */
+static struct grid grid;
+
+/* The instance of test case */
+CUTE_TestCase *case_rule;
+
+
+
+static void setUp(void) {
+	memset(grid.rule, 0, sizeof grid.rule);
+}
+
+
+void test_empty_rule(void) {
+	const char rule[] = "B/S";
+	char expected[sizeof grid.rule] = {0};
+
+	fprintf(stderr, "Testing compilation of rule %s\n", rule);
+	int rc = compile_grid_rule(&grid, rule);
+	CUTE_assertEquals(rc, 0);
+	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
+}
+
+void test_zero_rule(void) {
+	const char rule[] = "B0/S0";
+	char expected[sizeof grid.rule] = {0};
+	SET_BIT(expected, 0, 1);
+	SET_BIT(expected, 16, 1);
+
+	fprintf(stderr, "Testing compilation of rule %s\n", rule);
+	int rc = compile_grid_rule(&grid, rule);
+	CUTE_assertEquals(rc, 0);
+	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
+}
+
+void test_default_rule(void) {
+	const char rule[] = "B3/S23";
+	char expected[sizeof grid.rule] = {0};
+	// TODO work out expected
+
+	fprintf(stderr, "Testing compilation of rule %s\n", rule);
+	int rc = compile_grid_rule(&grid, rule);
+	CUTE_assertEquals(rc, 0);
+	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
+}
+
+void test_full_rule(void) {
+	const char rule[] = "B012345678/S012345678";
+	char expected[sizeof grid.rule];
+	memset(expected, -1, sizeof grid.rule);
+
+	fprintf(stderr, "Testing compilation of rule %s\n", rule);
+	int rc = compile_grid_rule(&grid, rule);
+	CUTE_assertEquals(rc, 0);
+	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
+}
+
+
+void build_case_rule(void) {
+	case_rule = CUTE_newTestCase("Tests for the rule compilation code", 4);
+	CUTE_setCaseBefore(case_rule, setUp);
+	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_empty_rule));
+	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_zero_rule));
+	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_default_rule));
+	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_full_rule));
+}
--- test/sdlife.c
+++ test/sdlife.c
@@ -6,17 +6,20 @@
 extern CUTE_TestCase *case_grid;
 extern void build_case_grid(void);
 extern void tests_bits(void);
+extern CUTE_TestCase *case_rule;
+extern void build_case_rule(void);
 
 int main() {
 	CUTE_RunResults **results;
 
 	build_case_grid();
+	build_case_rule();
 
-	CUTE_prepareTestSuite(1, case_grid);
+	CUTE_prepareTestSuite(2, case_grid, case_rule);
 
 	results = CUTE_runTestSuite();
 
-	CUTE_printResults(1, results);
+	CUTE_printResults(2, results);
 
 	tests_bits();
 
