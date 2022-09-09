b73746a546b8bb90c0d6b700f8a5a2df90d8aa85
joH1 <jiema@live.fr>
2022-09-09 22:57:01 +0200

    Add a new rule compilation test
    The test checks the compilation of the B01/S01 rule, that is the blocks
    corresponding to the digits 0 and 1, so none or a single live neighbor
    cell.

--- test/rule.c
+++ test/rule.c
@@ -39,6 +39,34 @@ void test_zero_rule(void) {
 	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
 }
 
+void test_zero_one_rule(void) {
+	const char rule[] = "B01/S01";
+	char expected[sizeof grid.rule] = {0};
+	SET_BIT(expected, 0, 1);
+	SET_BIT(expected, 16, 1);
+	SET_BIT(expected, 1, 1);
+	SET_BIT(expected, 2, 1);
+	SET_BIT(expected, 4, 1);
+	SET_BIT(expected, 8, 1);
+	SET_BIT(expected, 32, 1);
+	SET_BIT(expected, 64, 1);
+	SET_BIT(expected, 128, 1);
+	SET_BIT(expected, 256, 1);
+	SET_BIT(expected, 17, 1);
+	SET_BIT(expected, 18, 1);
+	SET_BIT(expected, 20, 1);
+	SET_BIT(expected, 24, 1);
+	SET_BIT(expected, 48, 1);
+	SET_BIT(expected, 80, 1);
+	SET_BIT(expected, 144, 1);
+	SET_BIT(expected, 272, 1);
+
+	fprintf(stderr, "Testing compilation of rule %s\n", rule);
+	int rc = compile_grid_rule(&grid, rule);
+	CUTE_assertEquals(rc, 0);
+	CUTE_assertEquals(memcmp(grid.rule, expected, sizeof grid.rule), 0);
+}
+
 void test_default_rule(void) {
 	const char rule[] = "B3/S23";
 	char expected[sizeof grid.rule] = {0};
@@ -63,10 +91,11 @@ void test_full_rule(void) {
 
 
 void build_case_rule(void) {
-	case_rule = CUTE_newTestCase("Tests for the rule compilation code", 4);
+	case_rule = CUTE_newTestCase("Tests for the rule compilation code", 5);
 	CUTE_setCaseBefore(case_rule, setUp);
 	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_empty_rule));
 	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_zero_rule));
+	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_zero_one_rule));
 	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_default_rule));
 	CUTE_addCaseTest(case_rule, CUTE_makeTest(test_full_rule));
 }
