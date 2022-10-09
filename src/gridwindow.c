47525aab4d250b2a9f1826972bddeec30fc8528b
Moonstroke <jiema@live.fr>
2022-10-10 00:05:36 +0200

    Fix the jittering when resizing the window
    The call to resize_grid_window inside handle_system_event was
    responsible for the window stuttering when resized. This call is simply
    replaced by a modification of the event's size parameters to let the
    window manager perform the resize, and a repaint call to update the
    content of the new zone.

--- src/gridwindow.c
+++ src/gridwindow.c
@@ -818,7 +818,9 @@ int handle_system_event(struct grid_window *gw, SDL_SysWMmsg *msg) {
 			return -__LINE__;
 		} else {
 			resize_grid(gw->grid, new_width, new_height);
-			resize_grid_window(gw);
+			rect->right = rect->left + new_width * (gw->border_width + gw->cell_pixels) + gw->border_width - 1;
+			rect->bottom = rect->top + new_height * (gw->border_width + gw->cell_pixels) + gw->border_width + 5;
+			render_grid_window(gw);
 		}
 	}
 #endif
